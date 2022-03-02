c8a604dc3c03d8866dc03bc1f6a4769d9f121876
pxie2016 <xie.zim@northeastern.edu>
2022-03-02 00:17:23 -0800

    Extra credit for Lecture 6

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+    return (
+        <body>
+        <div className="gridcontainer">
+            <div className="header">
+                <h1>Zimeng (Parker) Xie</h1>
+            </div>
+            <div className="leftsidebar"></div>
+            <div className="main">
+                <div className="mybox"></div>
+            </div>
+            <div className="rightsidebar"></div>
+            <div className="footer"></div>
+        </div>
+        </body>
+    )
+}
